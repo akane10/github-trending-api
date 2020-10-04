2dcc6106c57e7f160b2df8ddb784992a7a6661aa
yapie <targaryen.akane@gmail.com>
2020-10-04 21:05:10 +0700

    start server

--- src/api/index.js
+++ src/api/index.js
@@ -2,4 +2,5 @@ const express = require('express');
 const router = require('./route');
 const app = express();
 app.use(router);
+app.listen(8080, () => console.log('start on port 8080'));
 module.exports = app;
